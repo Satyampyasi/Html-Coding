ccdf8e278d4e5d7b949e0b54a4d3467c08218585
satyampyasi <satyampyasi565@gmail.com>
2024-12-05 11:17:41 +0530

    JS

---
+++ wrok_html/JAVAscript/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "satyampyasi@gamial.com"
+var accountPassword = "8220"
+accountCity = "city"
+
+// accountId = 2   Thdis is not allowed 
+accountEmail = "hae@JKJDOQ"
+accountPassword ="4545"
+accountCity =" hatta"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
