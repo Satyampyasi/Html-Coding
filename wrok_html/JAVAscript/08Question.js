d4fe456f8b37a5f5c8098827143c6edb174a3db5
satyampyasi <satyampyasi565@gmail.com>
2024-12-30 22:48:57 +0530

    project

---
+++ wrok_html/JAVAscript/08Question.js
@@ -0,0 +1,18 @@
+  function takecoffe(){
+     alert("1 coffee");
+     alert("2 cancel");
+  }
+
+
+function coffee(){
+  console.log("your coffe prepared");
+   Track1()
+}
+let timer=setTimeout(coffee,3000)
+
+function Track1(){
+ console.log("serves coffee");
+}
+
+
+// clearTimeout(timer)
