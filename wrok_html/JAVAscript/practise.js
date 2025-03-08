543881a4571baa6b2d7fa0db68ae91e5be16df47
satyampyasi <satyampyasi565@gmail.com>
2025-03-08 11:06:41 +0530

    revise

---
+++ wrok_html/JAVAscript/practise.js
@@ -0,0 +1,12 @@
+function abc(){
+     var a=12;
+     function def(){
+          var b=12;
+     }
+}
+abc() 
+
+var a = [15,12,5,41,78,6,5,89,44];
+a.forEach(function(val){
+     console.log(val+2);
+})
