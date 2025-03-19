88d897756257e2fa588f2988b256048af59425d3
satyampyasi <satyampyasi565@gmail.com>
2025-03-19 13:37:29 +0530

    new work

--- wrok_html/JAVAscript/Call_back.js
+++ wrok_html/JAVAscript/Call_back.js
@@ -1,11 +1,21 @@
  
-function one ( call_two){
-     console.log("Step 1 complete now call step 2")
-     call_two();
-};
+// function one ( call_two){
+//      console.log("Step 1 complete now call step 2")
+//      call_two();
+// };
 
 
-function two (){
-     console.log("Step 2")
+// function two (){
+//      console.log("Step 2")
+// }
+// one(two);
+
+let   stocks ={
+     fruits : [ "grapes", "orange","banana","apple"],
+     liquid :[ "maza","fruite","water","ice"],
+     holder : ["cone ","cup","stick"],
+     toppings:["chocolate","peanuts"]
 }
-one(two);
+
+let is_shop_open = true;
+
